cb22777589f7c3fa159ef37dfb8a3a3507c11d79
ahgddev <ahgddev@gmail.com>
2024-02-26 10:34:17 -0700

    Working on external helper functions.

--- src/pizzasindex.js
+++
@@ -1,3 +0,0 @@
-
-/* Note: DO NOT use API keys; this makes it more difficult for instructors to grade finished projects efficiently. */
-//Add a search bar?
---
+++ src/toppingsHelpers.js
@@ -0,0 +1,7 @@
+
+
+function makeUpdateForm(data){
+    let updateForm = document.createElement("form");
+    updateForm.setAttribute("id", "updateForm");
+    
+}
